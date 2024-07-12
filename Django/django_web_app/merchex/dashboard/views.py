f680d079befae63ec84b83f27a44c4474fc6c60b
Carolina SOMARRIBA <casomarr@paul-f4Ar5s1.clusters.42paris.fr>
2024-07-12 18:43:53 +0200

    commands folder added to create and clear random data for display tests in dashboard.html

--- Django/django_web_app/merchex/dashboard/views.py
+++ Django/django_web_app/merchex/dashboard/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-# from dashboard.models import User
+from dashboard.models import Stats
 from jess.models import User
 
 def dashboard_view(request):
     users = User.objects.all()
+    stats = Stats.objects.all().select_related('user') #foreign key : to link user to stats
     return render(request, 'dashboard.html',
-				{ 'users': users })
+				{'users': users, 'stats': stats})
--- Django/django_web_app/merchex/jess/models.py
+++ Django/django_web_app/merchex/jess/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.fields.CharField(max_length=100)
+    id = models.AutoField(primary_key=True) #id est une colonne qui existe par default dans Django
+    name = models.fields.CharField(max_length=100, null=True)
+    nickname = models.CharField(max_length=100, null=True)
+
